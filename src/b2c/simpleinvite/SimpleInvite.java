ef42e6900fc2fad628d2be7ac7501849fb834efe
fricaiAndlat <chloroplast@aikq.de>
2016-03-05 21:52:32 +0100

    basisplugin

--- src/b2c/simpleinvite/SimpleInvite.java
+++ src/b2c/simpleinvite/SimpleInvite.java
@@ -1,5 +1,38 @@
 package b2c.simpleinvite;
 
-public class SimpleInvite {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleInvite extends JavaPlugin implements Listener {
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this,  this);
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        if(!(sender instanceof Player)){
+            sender.sendMessage("this command can only be run by a player");
+            return false;
+        }
+
+        if(!cmd.getName().equalsIgnoreCase("mirror")){
+            return false;
+        }
+        Player player = (Player)sender;
+
+        return true;
+
+    }
 
 }
