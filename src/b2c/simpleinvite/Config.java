fed5a539234045e47a39664e36941c036e709faf
Moritz Bust <mbust@live.de>
2016-03-06 00:43:40 +0100

    Added some more config options

--- src/b2c/simpleinvite/Config.java
+++ src/b2c/simpleinvite/Config.java
@@ -10,7 +10,9 @@ public class Config {
     public static List<String> INVITATION_COMMANDS;
     public static int STRIKE_MOD;
     public static List<String> STRIKE_COMMANDS;
-
+    public static int INTERVAL_INVITES;
+    public static int INTERVAL_TIME;
+    public static int REASON_LENGTH;
 
     public static void load(FileConfiguration cfg) {
         cfg.addDefault("Invitation timeout in minutes", 60);
@@ -21,7 +23,12 @@ public class Config {
         STRIKE_MOD = cfg.getInt("Modulo strike amount");
         cfg.addDefault("Strike action", Arrays.asList("ban %user%", "say %user% has now %strikes% strikes and is being punished"));
         STRIKE_COMMANDS = cfg.getStringList("Strike action");
-
+        cfg.addDefault("Maximum invites per interval", 5);
+        INTERVAL_INVITES = cfg.getInt("Maximum invites per interval");
+        cfg.addDefault("Interval length", 30);
+        INTERVAL_TIME = cfg.getInt("Interval length");
+        cfg.addDefault("Maximum reason length", 32);
+        REASON_LENGTH = cfg.getInt("Maximum reason length");
     }
 
 }
