ba4b8d3634e87ccf0a3c4195aac6d1e1c4851a53
fricaiAndlat <chloroplast@aikq.de>
2016-03-05 23:33:51 +0100

    IO stuff

--- src/b2c/simpleinvite/Invite.java
+++ src/b2c/simpleinvite/Invite.java
@@ -1,12 +1,39 @@
 package b2c.simpleinvite;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Created by Peer on 05.03.2016.
  */
 public class Invite {
 
-    long guarantorID;
-    long timestamp;
-    String playerName;
+    public final static ArrayList<Invite> INVITATIONEN = new ArrayList<Invite>();
+
+    public final long guarantorID;
+    public final long timestamp;
+    public final String playerName;
+
+    public Invite(long guarantorID, long timestamp, String playerName) {
+        this.guarantorID = guarantorID;
+        this.timestamp = timestamp;
+        this.playerName = playerName;
+    }
+
+
+    public static Invite read(DataInputStream input) throws IOException {
+        long guarantorID = input.readLong();
+        long timestamp = input.readLong();
+        String playerName = input.readUTF();
+        return new Invite(guarantorID, timestamp, playerName);
+    }
+
+    public void write(DataOutputStream output) throws IOException {
+        output.writeLong(guarantorID);
+        output.writeLong(timestamp);
+        output.writeUTF(playerName);
+    }
 
 }
--- src/b2c/simpleinvite/SimpleInvite.java
+++ src/b2c/simpleinvite/SimpleInvite.java
@@ -1,5 +1,6 @@
 package b2c.simpleinvite;
 
+import b2c.simpleinvite.io.Loader;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
@@ -7,15 +8,46 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+
 public class SimpleInvite extends JavaPlugin implements Listener {
+
+    Loader dataLoader;
+
     @Override
     public void onDisable() {
-
+        try {
+            dataLoader.writeData();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't save dataFile");
+        }
     }
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this,  this);
+        File savedData = new File(this.getDataFolder(), "SimpleInviteData");
+        if(!savedData.exists()){
+            this.getDataFolder().mkdirs();
+
+            try {
+                savedData.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Can't create data file");
+            }
+        }
+
+        dataLoader = new Loader(savedData);
+        try {
+            dataLoader.readData();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't read dataFile");
+        }
+
     }
 
 
